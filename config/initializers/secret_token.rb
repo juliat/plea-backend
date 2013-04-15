90852e7367d3ad9197229b491c9be09d95fa747f
Julia Teitelbaum <julialt@gmail.com>
2013-04-15 01:10:21 -0400

    generated new rails app, modified Gemfile and database.yml config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PleaBackend::Application.config.secret_token = 'a4a4eae455880df5d3c3f8f2a778110f281f3e140c62e866f754d4b048374bec1bcea6c196d5aa72cb40b8d8aea831114dc03a7c94dd8a09a14cb9be8fdb7518'
